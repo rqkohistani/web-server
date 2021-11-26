5cad8535f4f112395d4ba9b0410ccfd30d2a72b7
Rashed Qazizada <rashidqk@gmail.com>
2021-11-26 20:40:17 +0100

    bug fixed

--- src/app.js
+++ src/app.js
@@ -38,10 +38,11 @@ app.get('', (req, res) => {
     title: 'Weather app',
     name: 'Rashed'
   },
+  
   geocode(req.body.location, (response,{
     latitude, longitude, location } = {}
   ))
-
+  
   )
 })
 
@@ -63,13 +64,13 @@ app.get('/help', (req, res) => {
 })
 
 
-// app.get('/weather', (req, response) => {
-//   // res.send('Weather: app.js')
-//   geocode(req.body.location, (response,{
-//     latitude, longitude, location } = {}
-//   ))
+app.get('/weather', (req, response) => {
+  // res.send('Weather: app.js')
+  geocode(req.body.location, (response,{
+    latitude, longitude, location } = {}
+  ))
 
-// })
+})
 
 // 3. about pattern match
 app.get('/about/*', (req, res) => {
