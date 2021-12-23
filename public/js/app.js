b463df8a797f3f659a170f34a2d4149602ab260d
Rashed Qazizada <raq@softhouse.se>
2021-12-23 08:43:23 +0100

    fixed bugs

--- public/js/app.js
+++ public/js/app.js
@@ -20,20 +20,20 @@ weatherForm.addEventListener("submit", (e) => {
 
   const unitsIn = "units=m"; //by default is m check the weatherstack documentation for info f=fahrenheit, m=metric celsius
   const access_key = "47b0ec59a41936fddce4544b239349ab";
-  // const url = `http://api.weatherstack.com/current?access_key=${access_key}&query=${location}&${unitsIn}`;
+  const url = `http://api.weatherstack.com/current?access_key=${access_key}&query=${location}&${unitsIn}`;
   // const url =  "http://api.weatherstack.com/current?access_key=" +    access_key +    "&query=" +    location;
-  const url = "/current?access_key=" + access_key + "&query=" + location;
-
-  fetch(url).then((response) => {
-    console.log("test");
-    messageTwo.textContent = response.json()
-    response.json().then((data) => {
-      if (data.error) {
-        messageOne.textContent =
-          location + ": Unable to find location. Try another search e.g Malmo ";
-      } else {
-        messageOne.textContent = `Temperatures: ${data.current.temperature} => Wind speed: ${data.current.wind_speed} => Humidity: ${data.current.humidity} => Feels like: ${data.current.feelslike} => cloud cover: ${data.current.cloudcover} => wind speed : ${data.current.wind_speed} => latitude: ${data.location.lat} => longtitude: ${data.location.lon} `;
-      }
-    });
+  // const url = "/current?access_key=" + access_key + "&query=" + location;
+//api.weatherstack.com/current?access_key=47b0ec59a41936fddce4544b239349ab&query=Kabul
+http: fetch(url).then((response) => {
+  console.log("test");
+  // messageTwo.textContent = response.json()
+  response.json().then((data) => {
+    if (data.error) {
+      messageOne.textContent =
+        location + ": Unable to find location. Try another search e.g Malmo ";
+    } else {
+      messageOne.textContent = `Temperatures: ${data.current.temperature} => Wind speed: ${data.current.wind_speed} => Humidity: ${data.current.humidity} => Feels like: ${data.current.feelslike} => cloud cover: ${data.current.cloudcover} => wind speed : ${data.current.wind_speed} => latitude: ${data.location.lat} => longtitude: ${data.location.lon} `;
+    }
   });
+});
 });
